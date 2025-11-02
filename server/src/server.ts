c7a759bb33d209c48ba0f56433da784775aa4470
Sindre Haavaldsen <sindre.h@outlook.com>
2025-11-02 13:08:13 +0100

    feat: open cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,9 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import productsRoutes from './routes/products.routes';
 import ordersRoutes from './routes/orders.routes';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.use(bodyParser.json());
 
