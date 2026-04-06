c7179b7d28725e8f427ecdbae1c3e81aa57613b5
SindreHa <sindre.h@outlook.com>
2026-04-06 14:17:08 +0200

    chore: remove unused form model

--- src/app/pages/cart/order-submit-modal/model/customer-details-form.modal.ts
+++
@@ -1,6 +0,0 @@
-import { CustomerDetails } from '../../../../api/model/order.model';
-import { FormControl } from '@angular/forms';
-
-export type CustomerDetailsForm = {
-    [K in keyof CustomerDetails]: FormControl<CustomerDetails[K]>;
-};
